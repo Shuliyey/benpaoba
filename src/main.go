5b9451da689637bd9099b50935c7ade29461c927
Zeyu Ye <z.ye@base2services.com>
2018-01-12 15:11:10 +1100

    update main and task

--- src/main.go
+++ src/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
   "fmt"
+  "./task"
 )
 
 func main() {
   fmt.Println("奔跑吧")
+  task.Hey()
 }
--- src/task/task.go
+++ src/task/task.go
@@ -1,1 +1,9 @@
 package task
+
+import (
+  "fmt"
+)
+
+func Hey() {
+  fmt.Println("package task")
+}
