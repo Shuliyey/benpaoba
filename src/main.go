899775cebaf9135e3d68d596ce95e2709d9cc453
Zeyu Ye <z.ye@base2services.com>
2018-01-12 15:56:51 +1100

    update benpaoba version

--- src/main.go
+++ src/main.go
@@ -11,6 +11,7 @@ func main() {
   app := cli.NewApp()
   app.Name = "奔跑吧(bēn pǎo ba)"
   app.Usage = "脚本执行"
+  app.Version= "0.0.1"
   app.Action = func(c *cli.Context) error {
     fmt.Println("伐木累")
     return nil
